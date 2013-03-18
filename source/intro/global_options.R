7611fa52d4c7b70e4a6346d0b2f76eb11b104fac
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-03-18 14:22:34 +1100

    Work around to have output formatted differently to input.

--- source/intro/global_options.R
+++ source/intro/global_options.R
@@ -1,3 +1,6 @@
 opts_chunk$set(tidy=FALSE, fig.height=5, comment="")
 options(show.signif.stars=FALSE)
 options(max.print=170)
+render_markdown()
+knit_hooks$set(output=function(x, options)
+               paste0("\n\n```plain\n", x, "```\n\n"))
