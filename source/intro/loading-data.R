dcad44fccf25a81aa06087e17abffeb7ed2570dc
Rich FitzJohn <rich.fitzjohn@gmail.com>
2013-03-18 15:02:34 +1100

    More on data.frames.

--- source/intro/loading-data.R
+++ source/intro/loading-data.R
@@ -154,10 +154,14 @@ data[[v]]
 
 ### plus abbrviated names and the dangers they can cause.
 
-## This returns a `data.frame` with one column
-data["Plot"]
-## This returns a `data.frame` with two columns
-data[c("Plot", "Weight")]
+## This returns a `data.frame` with one column:
+head(data["Plot"])
+## This returns a `data.frame` with two columns:
+head(data[c("Plot", "Weight")])
+## (I'm just using `head` here to keep the output under control.  If
+## you actually wanted a `data.frame` like this you might do
+data.sub <- data[c("Plot", "Weight")]
+## and then continue to use the new `data.sub` object).
 
 ## The difference between `[` and `[[` can be confusing.
 
@@ -180,23 +184,39 @@ data[c(1, 5, 10),]
 
 ## ## Be careful with indexing by location
 
-## The above all index by *name* or by *location* (index).  Avoid
-## doing things like this:
+## The above all index by *name* or by *location* (index).  However,
+## you generally want to avoid referencing by number in your saved
+## code, e.g.:
 data.height <- data[[5]]
 
-## Because if you change the order of your spreadsheet (add or delete
-## a column), everything that depends on `data.height` may change.
-## You may see people do this:
+## This is because if you change the order of your spreadsheet (add or
+## delete a column), everything that depends on `data.height` may
+## change.  You may also see people do this in their code.
 data.height <- data[,5]
 
 ## This should really be avoided.  By name is much more robust and
-## easy to read.
+## easy to read later on, even if it is more typing at first.
 data.height <- data$Height
 data.height <- data[["Height"]]
 
-## When should you index by number?
+## When should you index by location?
 
-## When you are *computing* the indices.
+## When you are *computing* the indices.  As an example: suppose that
+## you wanted every other row (perhaps you're trying to generate a
+## nonrandom some sample of data?)  Remember `seq` from above?  We can
+## generate a sequnce of integers 1, 3, ..., up to the last (or second
+## to last) row in our data set like this:
+idx <- seq(1, nrow(data), by=2)
+## Then subset like this:
+data.oddrows <- data[idx,]
+
+## Our new data set has half the rows of the old data set:
+nrow(data.oddrows)
+nrow(data)
+
+## Because row names are preserved, you can see the odd numbers in the
+## row names.
+head(data.oddrows)
 
 ## ## Indexing by logical vector
 
@@ -206,14 +226,15 @@ data.height <- data[["Height"]]
 data != data2
 
 ## There is one entry in the `Height` row that disagrees.  How can we
-## get the whole line that it is on?
+## extract the line that the mismatch is on?
 
 ## We could do it by index:
 data[20,]
 data2[20,]
 
 ## But that requires us to look for the error, note the row, write it
-## down, etc.  Boring, and computers are less error prone than humans.
+## down, etc.  Boring, and computers are less error prone than
+## humans.  Plus, I just said that we should not do that.
 
 ## This is a logical vector that indicates where the entries in vector
 ## 1 disagree with vector 2:
@@ -224,8 +245,15 @@ data.differ <- data$Height != data2$Height
 data[data.differ,]
 data2[data.differ,]
 
+## You can convert from a logical (`TRUE`/`FALSE`) vector to an
+## integer vector with the `which` function:
+which(data.differ)
+
+## This can be really useful.
+
 ## ### Excercise:
-## Return all the rows in `data` where the data agree.
+## 1. Return all the rows in `data` where the data agree.
+## 2. Return all the rows in `data` from `plot-8`
 
 ## #### A solution:
 data.same <- data$Height == data2$Height
@@ -235,10 +263,73 @@ data[data.same,]
 data[!data.differ,]
 ## read `!x` as "not x",
 
+## ### Exercise:
+
 ## Subsetting can be useful when you want to look at bits of your
 ## data.  For example, all the rows where the Height is at least 10
 ## and there was no seed herbivore:
 data[data$Height > 10 & data$Seed.herbivore,]
+
+## The `&` operator here is a logical "and" (read `x & y` as "x and
+## y"):
+
+##  * `TRUE  & TRUE`  is `TRUE`
+##  * `TRUE  & FALSE` is `FALSE`
+##  * `FALSE & TRUE`  is `FALSE`
+##  * `FALSE & FALSE` is `FALSE`
+
+## In contrast, the `|` operator is a logical "or" (read as "or")
+
+##  * `TRUE  | TRUE`  is `TRUE`
+##  * `TRUE  | FALSE` is `TRUE`
+##  * `FALSE | TRUE`  is `TRUE`
+##  * `FALSE | FALSE` is `FALSE`
+
+## The other, less common, operator is the exclusive or:
+
+##  * `xor(TRUE,  TRUE)`  is `FALSE`
+##  * `xor(TRUE,  FALSE)` is `TRUE`
+##  * `xor(FALSE, TRUE)`  is `TRUE`
+##  * `xor(FALSE, FALSE)` is `FALSE`
+
+## So you can do all sorts of crazy things like
+data[data$Plot == "plot-2" & data$Seed.herbivore & data$Root.herbivore,]
+## and get all the cases in plot 2 where there were both seed
+## herbivores and root herbivores.  Or
+data[data$Height > 75 & (data$Seed.herbivore | data$Root.herbivore),]
+## and get all the plants that are quite tall in treatments with
+## *either* a seed herbivore or a root herbivore (or both).
+## 
+## You can build these up if you want:
+idx.tall <- data$Height > 75
+idx.herbivore <- data$Seed.herbivore | data$Root.herbivore
+idx.select <- idx.tall & idx.herbivore
+data[idx.select,]
+
+## whatever you find easiest to read and write.
+
+## > Programs should be written for people to read, and only
+## > incidentally for machines to execute (Structure and
+## > Interpretation of Computer Programs" by Abelson and Sussman)
+
+## ### The `subset` function to simplify writing complex subsets
+
+## There is a function `subset` that may help you write complex
+## subsets.
+subset(data, Height > 75 & (Seed.herbivore | Root.herbivore))
+
+## This can help, especially interactively, but it can also bite you.
+## It is not always obvious where the "value" of the variables in the
+## second argument are coming from.  For example:
+subset(data, idx.tall & (Seed.herbivore | Root.herbivore))
+## This works fine, because it found `idx.tall`.  So when you read
+## your code, you need to think carefully about which values are
+## coming from the `data.frame` and which are coming from elsewhere.
+
+## This is an unfortunate example of a function designed to be used by
+## beginners, but it only really understandable once you understand
+## more of what is going on.  You'll see it used widely, and it can
+## simplify things.  But be careful.
 
 ## ## Indexing need not make things smaller
 
